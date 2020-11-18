89bfa2aa12b4ae3135e446b47bcc19efd83412bb
froemosen <kristianantonhedegaard@gmail.com>
2020-11-18 23:00:50 +0100

    Input af ligning
    MANGLER:
    - Udskrivning - kommentar er sat
    - Optimering - Mere præcise værdier (Bruger vi Deltax i stedet for stigning?)

--- Differencialregning/Differencialregning.py
+++ Differencialregning/Differencialregning.py
@@ -1,86 +1,123 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy
 
+print("\n_______NU STARTER PROGRAM TIL DIFFENRENCIALREGNING_______\n")
 
-xAkseLen = int(input("Længde på x-aksen i begge retninger: "))
-plt.grid()
-newx = -xAkseLen
-for value in range(xAkseLen*100*2):
-    oldx = newx
-    oldfx = oldx**2+10*oldx+10
-    newx = oldx+0.01
-    newfx = newx**2+10*newx+10
-    plt.plot([oldx, newx], [oldfx, newfx])
-    
+print("Guide til ligning:\n    Indtast ligning på en de måder som ses i eksempler herunder:\n        - Python-sprog: a*x**2+b\n        - Normal-sprog: ax^2+b\n        - Blanding: a*x^2+b\n")
 
+def decode():
+    ligningString = ""
+    prevTegn = ""
+    ligningList = list(input("Indtast ligning: "))
+    for tegn in ligningList:
+        if tegn == "^":
+            tegn = "**"
+        if tegn.isalpha():
+            tegn = "x"
+        
+        if prevTegn.isdigit() and tegn.isalpha():
+            prevTegn = "*"
+        else:
+            prevTegn = ""
+        
+        ligningString += prevTegn
+        ligningString += tegn
+        prevTegn = tegn
 
-
-xTangent = float(input("Punkt på funktionen hvor du vil finde hældning: "))
-deltax = xTangent*5 #Start deltax
-prevDeltax = deltax+1
-
-timeToPause = 0.5
-
-for execution in range(1000):
-    deltax /= 2
-
-    y1 = xTangent**2+10*xTangent+10
-    y2 = (xTangent+deltax)**2+10*(xTangent+deltax)+10
-    x1 = xTangent
-    x2 = xTangent+deltax
-
-    try:
-        stigning = (y2-y1)/(x2-x1)
-        if prevDeltax-deltax < 10**(-16) and prevDeltax-deltax > 0:
-            print("a =", stigning)
-            break
-
-        elif prevDeltax-deltax > -(10**(-16)) and abs(prevDeltax)-abs(deltax) < 0:
-            print("a =", stigning)
-            break
-    except:
-        stigning = (y2-y1)/(x2-x1)
-        if prevDeltax-deltax < 10**(-8) and prevDeltax-deltax > 0:
-            print("a =", stigning)
-            break
-
-        elif prevDeltax-deltax > -(10**(-8)) and prevDeltax-deltax < 0:
-            print("a =", stigning)
-            break
-    finally:
-        stigning = (y2-y1)/(x2-x1)
-        if prevDeltax-deltax < 10**(-4) and prevDeltax-deltax > 0:
-            print("a =", stigning)
-            break
-
-        elif prevDeltax-deltax > -(10**(-4)) and prevDeltax-deltax < 0:
-            print("a =", stigning)
-            break
-
-    try:
-        line = tangent.pop(0)
-        line.remove()
-    except:
-        pass
-
-    b=(y1)-(stigning)*(x1)
-    bundTangentX = xAkseLen
-    topTangentX = -xAkseLen
-    bundTangentY = stigning*xAkseLen+b
-    topTangentY = stigning*-(xAkseLen)+b
-
-    tangent = plt.plot([bundTangentX, topTangentX], [bundTangentY, topTangentY])
+    ligningReady = sympy.sympify(ligningString)
+    print("Ligningen i python-sprog: ", ligningReady)
+    printGraf(ligningReady)
 
     
 
-    timeToPause /= 1.4
-    plt.pause(timeToPause)
+def printGraf(ligningReady):
+    xAkseLen = int(input("\nLængde på x-aksen i begge retninger: "))
+    plt.grid()
+    newx = -xAkseLen
+    for value in range(xAkseLen*100*2):
+        oldx = newx
+        oldfx = ligningReady.subs(dict(x=oldx))
+        newx = oldx+0.01
+        newfx = ligningReady.subs(dict(x=newx))
+        plt.plot([oldx, newx], [oldfx, newfx])
     
-    prevDeltax = deltax
+    lavTangent(xAkseLen, ligningReady)
+    
+def lavTangent(xAkseLen, ligningReady):
+    xTangent = float(input("\nPunkt på funktionen hvor du vil finde hældning: "))
+    deltax = xTangent*5+1 #Start deltax
+    prevDeltax = deltax+1
+    timeToPause = 0.5
 
+    for execution in range(1000):
+        deltax /= 2
 
-plt.show()
-print("\nDin funktion bliver nu indtegnet i et koordinatsystem\n")
+        y1 = ligningReady.subs(dict(x=xTangent))
+        y2 = ligningReady.subs(dict(x=xTangent+deltax))
+        x1 = xTangent
+        x2 = xTangent+deltax
+
+        try:
+            stigning = (y2-y1)/(x2-x1)
+            b=(y1)-(stigning)*(x1)
+            if prevDeltax-deltax < 10**(-20) and prevDeltax-deltax > 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+
+            elif prevDeltax-deltax > -(10**(-20)) and abs(prevDeltax)-abs(deltax) < 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+        except:
+            stigning = (y2-y1)/(x2-x1)
+            b=(y1)-(stigning)*(x1)
+            if prevDeltax-deltax < 10**(-8) and prevDeltax-deltax > 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+
+            elif prevDeltax-deltax > -(10**(-8)) and prevDeltax-deltax < 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+        finally:
+            stigning = (y2-y1)/(x2-x1)
+            b=(y1)-(stigning)*(x1)
+            if prevDeltax-deltax < 10**(-4) and prevDeltax-deltax > 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+
+            elif prevDeltax-deltax > -(10**(-4)) and prevDeltax-deltax < 0:
+                print("\nHældningstal i punkt:  a =", stigning)
+                print("Tangentensligning:     t(x) = " + str(stigning)+"x"+str(b)) #Nyt symbol i stedet for x?
+                break
+
+        try:
+            line = tangent.pop(0)
+            line.remove()
+        except:
+            pass
+
+        
+        bundTangentX = xAkseLen
+        topTangentX = -xAkseLen
+        bundTangentY = stigning*xAkseLen+b
+        topTangentY = stigning*-(xAkseLen)+b
+
+        
+
+        tangent = plt.plot([bundTangentX, topTangentX], [bundTangentY, topTangentY])
+        timeToPause /= 1.5
+        prevDeltax = deltax
+
+        plt.pause(timeToPause)
+        
+if __name__ == '__main__':
+    decode()
+    plt.show()
 """
 plt.axis([0, 10, 0, 1])
 
--- Differencialregning/Start.bat
+++ Differencialregning/Start.bat
@@ -1,3 +1,4 @@
 pip install matplotlib
 pip install numpy
+pip install sympy
 python D:\SO4---MatProg\Differencialregning\Differencialregning.py

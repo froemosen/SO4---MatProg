b1cd2f7605987c29ce2396a7a3be7574e0becb76
Lacvuong02 <60395679+Lacvuong02@users.noreply.github.com>
2020-11-20 12:29:19 +0100

    Hello There

--- Gui diff og integral/main.py
+++ Gui diff og integral/main.py
@@ -5,14 +5,21 @@ window = Tk()
 window.title("Diffrencial og Intergral")
 window.geometry("500x250")
 
-main_frame = tk.Frame(window)
 
-Differencial = tk.Button(window, text = "Differencialregning")
-Differencial.grid(column = 0, row = 0)
+main_frame = tk.Frame(window)
+Titel = Label(window, text = "Insert titel", font=("Arial Bold", 25), padx = 250, pady = 50)
+Titel.pack()
+
+def clickone():
+    Titel.configure(text = "Hello")
+
+def clicktwo():
+    Titel.configure(text = "There")
+
+Differencial = tk.Button(window, text = "Differencialregning", command = clickone, height = 2, width = 15, padx = 50)
 Differencial.pack()
 
-Intergral = tk.Button(window, text = "Intergralregning")
+Intergral = tk.Button(window, text = "Intergralregning", command = clicktwo, height = 2, width = 15, padx = 50)
 Intergral.pack()
 
 window.mainloop()
-

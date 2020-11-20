aafdaf127ea1c83f61a37981337f4ec8bdeae549
Lacvuong02 <60395679+Lacvuong02@users.noreply.github.com>
2020-11-20 10:37:55 +0100

    GUI start up

--- Gui diff og integral/main.py
+++ Gui diff og integral/main.py
@@ -2,8 +2,17 @@ import tkinter as tk
 from tkinter import *
 
 window = Tk()
+window.title("Diffrencial og Intergral")
+window.geometry("500x250")
 
 main_frame = tk.Frame(window)
+
+Differencial = tk.Button(window, text = "Differencialregning")
+Differencial.grid(column = 0, row = 0)
+Differencial.pack()
+
+Intergral = tk.Button(window, text = "Intergralregning")
+Intergral.pack()
 
 window.mainloop()
 

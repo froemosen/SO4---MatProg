221669ac24d0dc0e46702facd9f57c3fa9e73980
Lacvuong02 <60395679+Lacvuong02@users.noreply.github.com>
2020-11-20 13:03:34 +0100

    Update main.py

--- Gui diff og integral/main.py
+++ Gui diff og integral/main.py
@@ -1,25 +1,35 @@
 import tkinter as tk
 from tkinter import *
+from tkinter.ttk import *
 
 window = Tk()
-window.title("Diffrencial og Intergral")
+# window.title("Diffrencial og Intergral")
 window.geometry("500x250")
+
+def OpenDifferencialWindow():
+    DifferencialWindow = Toplevel(window)
+    DifferencialWindow.title("Differencial Regning")
+    DifferencialWindow.geometry("500x250")
+    Label(DifferencialWindow, text = Something)
+
+def OpenIntergralWindow():
+    IntergralWindow = Toplevel(window)
+    IntergralWindow.title("Intergral Regning")
+    IntergralWindow.geometry("500x250")
+    Label(IntergralWindow, text = Something)
 
 
 main_frame = tk.Frame(window)
-Titel = Label(window, text = "Insert titel", font=("Arial Bold", 25), padx = 250, pady = 50)
+Titel = Label(window, text = "Insert titel")
 Titel.pack()
 
-def clickone():
-    Titel.configure(text = "Hello")
-
-def clicktwo():
-    Titel.configure(text = "There")
-
-Differencial = tk.Button(window, text = "Differencialregning", command = clickone, height = 2, width = 15, padx = 50)
+Differencial = tk.Button(window, text = "Differencialregning", command = OpenDifferencialWindow, height = 2, width = 15, padx = 50)
 Differencial.pack()
 
-Intergral = tk.Button(window, text = "Intergralregning", command = clicktwo, height = 2, width = 15, padx = 50)
+Intergral = tk.Button(window, text = "Intergralregning", command = OpenIntergralWindow, height = 2, width = 15, padx = 50)
 Intergral.pack()
+
+Indtastning = tk.Entry(window,width = 50)
+Indtastning.pack()
 
 window.mainloop()

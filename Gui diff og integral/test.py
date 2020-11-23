026c15806bf92e408c1924e5cc596d68a10b3c8f
hodyboi <60390730+hodyboi@users.noreply.github.com>
2020-11-23 10:33:42 +0100

    Create test.py

---
+++ Gui diff og integral/test.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+#--main vindue---
+main_window = tk.Tk()
+main_window.title("hAALLLOO")
+main_window.geometry('400x400')
+
+
+#----frame1----
+frame1=tk.Frame(main_window)
+tekst = tk.Label(frame1, text="halloe" )
+tekst.grid()
+
+
+frame1.grid()
+
+
+#---frame2---
+frame2=tk.Frame(main_window)
+tekst2 = tk.Label(frame2, text="halloe" )
+tekst2.grid()
+
+frame2.grid()
+
+
+
+
+main_window.mainloop()

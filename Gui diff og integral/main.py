5ee646d7e94e5fa896fe3c04f3d10001c63af2ab
Lacvuong02 <60395679+Lacvuong02@users.noreply.github.com>
2020-11-23 10:03:37 +0100

    Update main.py

--- Gui diff og integral/main.py
+++ Gui diff og integral/main.py
@@ -38,8 +38,8 @@ class MainFrame(tk.Frame):
         Box.pack(side = "top", fill = "both", expand= True)
 
         MainMenuWindow.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
-        DifferencialWindow.place(in_= Box, x = 0, y = 0, relwidth = 1, relhight = 1)
-        IntergralWindow.place(in_= Box, x = 0, y = 0, relwidth = 1, relhight = 1)
+        DifferencialWindow.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
+        IntergralWindow.place(in_= Box, x = 0, y = 0, relwidth = 1, relheight = 1)
 
         MainMenuButton = tk.Button(ButtonFrame, text = "Main Menu", command = MainMenuWindow.lift)
         DifferencialButton = tk.Button(ButtonFrame, text = "Differencial Regning", command = DifferencialWindow.lift) 
@@ -57,37 +57,3 @@ if __name__ == "__main__":
     main.pack(side = "top", fill = "both", expand = True)
     base.wm_geometry("500x500")
     base.mainloop()
-
-"""
-window = Tk()
-# window.title("Diffrencial og Intergral")
-window.geometry("500x250")
-
-def OpenDifferencialWindow():
-    DifferencialWindow = Toplevel(window)
-    DifferencialWindow.title("Differencial Regning")
-    DifferencialWindow.geometry("500x250")
-    Label(DifferencialWindow, text = "Something")
-
-def OpenIntergralWindow():
-    IntergralWindow = Toplevel(window)
-    IntergralWindow.title("Intergral Regning")
-    IntergralWindow.geometry("500x250")
-    Label(IntergralWindow, text = "Something")
-
-
-main_frame = tk.Frame(window)
-Titel = Label(window, text = "Insert titel")
-Titel.pack()
-
-Differencial = tk.Button(window, text = "Differencialregning", command = OpenDifferencialWindow, height = 2, width = 15, padx = 50)
-Differencial.pack()
-
-Intergral = tk.Button(window, text = "Intergralregning", command = OpenIntergralWindow, height = 2, width = 15, padx = 50)
-Intergral.pack()
-
-Indtastning = tk.Entry(window,width = 50)
-Indtastning.pack()
-
-window.mainloop()
-"""

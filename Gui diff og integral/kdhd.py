de61fc7f43f81ac302a78335f455fb3fc928f9ff
hodyboi <60390730+hodyboi@users.noreply.github.com>
2020-11-24 10:01:01 +0100

    Update kdhd.py

--- Gui diff og integral/kdhd.py
+++ Gui diff og integral/kdhd.py
@@ -2,11 +2,11 @@ from tkinter import *
 
 root =Tk()
 
-canvas = Canvas(root, width = 100, height = 100,)
+canvas = Canvas(root, width = 400, height = 400,)
 
 canvas.pack()
 
-img = PhotoImage(file = "C\Users\Lenovo\Documents\GitHub\SO4---MatProg\Gui diff og integral\fodboldja.png")
+img = PhotoImage(file = "fodboldja.png")
 
 canvas.create_image(20,20, anchor = NW, image=img)
 

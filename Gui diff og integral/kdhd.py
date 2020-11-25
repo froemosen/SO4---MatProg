6010bfd0d4e4651341a06ce378ebd82caa68b235
hodyboi <60390730+hodyboi@users.noreply.github.com>
2020-11-25 12:08:23 +0100

    Update kdhd.py

--- Gui diff og integral/kdhd.py
+++ Gui diff og integral/kdhd.py
@@ -5,14 +5,9 @@ root =tk.Tk()
 canvas = tk.Canvas(root, width = 400, height = 400,)
 
 
-
 img = tk.PhotoImage(file = "Gui diff og integral/fodboldja.png")
 
+canvas.create_image(100,100, image=img)
 
-#canvas.create_image(100,100, image=img)
-
-hej = tk.Label(root, bg = 'img')
-hej.pack()
-
-#canvas.pack()
+canvas.pack()
 tk.mainloop()
